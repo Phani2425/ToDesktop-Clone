bf7dc079ee05794effc79fb146f18c693628f980
Phani2425 <mohantyphanibhusan@gmail.com>
2024-05-29 23:25:57 +0530

    Create nav bar amd hero section

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {     
+      fontFamily: {
+        'display': ['Poppins', 'sans-serif'],
+        'body': ['Inter', 'sans-serif']
+    },
+    colors: {
+      'primary':'#3238f2',
+    },},
+  },
+  plugins: [],
+}
